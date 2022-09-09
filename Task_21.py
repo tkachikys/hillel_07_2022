d2ec436c6a88a3a21aaa15d8a1ebf655a1e9651f
tkachikys <110057926+tkachikys@users.noreply.github.com>
2022-09-09 18:46:40 +0300

    Дз. 21

---
+++ Task_21.py
@@ -0,0 +1,71 @@
+# Для рассмотренного на уроке класса Circle реализовать метод производящий вычитание двух окружностей,
+# вычитание радиусов произвести по модулю. Если вычитаются две окружности с одинаковым значением радиуса,
+# то результатом вычитания будет точка класса Point.
+import math
+
+
+class Point():
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def distance_from_origin(self):
+        return math.hypot(self.x, self.y)
+
+
+class Circle(Point):
+
+    def __init__(self, radius, x=0, y=0):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __str__(self):
+        return super().__str__()[:-1] + f', radius={self.radius})'
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        radius = abs(self.radius - other.radius)
+        if radius == 0:
+            print(f"Point{x},{y}")
+        else:
+            return f"Point {x},{y} radius: {radius}"
+
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        radius = self.radius + other.radius
+        return Circle(radius, x, y)
+
+    def edge_distance_from_origin(self):
+        return abs(self.distance_from_origin() - self.radius)
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
+
+    def area(self):
+        return math.pi * (self.radius**2)
+
+circle_1 = Circle(2, 4, 6)
+circle_2 = Circle(4, 4, 6)
+circle_3 = Circle(2, 2, 1)
+a = circle_2 - circle_1
+b = circle_3 - circle_1
+print(a)
+print(b)
